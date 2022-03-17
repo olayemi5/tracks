80702d8fd3aa10148427fc884ad156c6366acf15
Stephen Olayemi <olayemistephen007@gmail.com>
2022-03-17 12:41:24 +0100

    Work in progress

---
+++ src/component/AuthForm.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { View, StyleSheet } from "react-native";
+import { Text, Button, Input } from "react-native-elements";
+import Spacer from "./Spacer";
+
+const AuthForm = ({ headerText, errorMessage, onSubmit, submitButtonText }) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    return <View>
+                <Spacer>
+                <Text h3>{headerText}</Text>
+                </Spacer>
+                <Spacer />
+                <Input 
+                    autoComplete={false}
+                    autoCapitalize='none'
+                    label="Email" 
+                    value={email} 
+                    onChangeText={setEmail}
+                />
+                <Spacer />
+                <Input 
+                    secureTextEntry
+                    autoComplete={false}
+                    autoCapitalize='none'
+                    label="Password" 
+                    value={password} 
+                    onChangeText={setPassword}
+                />
+               { errorMessage ? <Text style={styles.errorMsg}> {errorMessage}  </Text> : null }
+                <Spacer>
+                    <Button onPress={() => onSubmit({ email, password })} title={submitButtonText}/>
+                </Spacer>
+    </View>
+}
+
+const styles = StyleSheet.create({
+     errorMsg: {
+        fontSize: 16,
+        color: 'red',
+        textAlign: 'center'
+    }
+})
+
+export default AuthForm
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,45 +1,26 @@
-import React, { useState, useContext } from 'react'
-import { View, StyleSheet, SafeAreaView, TouchableOpacity} from 'react-native'
-import { Text, Input, Button } from 'react-native-elements'
-import Spacer from '../component/Spacer'
+import React, { useContext } from 'react'
+import { StyleSheet, SafeAreaView} from 'react-native'
 import { Context as AuthContext} from '../context/AuthContext'
+import AuthForm from '../component/AuthForm'
+import NavLink from '../component/NavLink'
 
 const SignupScreen = ({ navigation }) => {
 
     const {state, signup} = useContext(AuthContext)
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+   
 
-    return <SafeAreaView 
-                style={styles.container}
-            >
-                <Spacer>
-                    <Text h3>Sign Up for Tracker</Text>
-                </Spacer>
-                <Spacer />
-                <Input 
-                    autoComplete={false}
-                    autoCapitalize='none'
-                    label="Email" 
-                    value={email} 
-                    onChangeText={setEmail}
-                />
-                <Spacer />
-                <Input 
-                    secureTextEntry
-                    autoComplete={false}
-                    autoCapitalize='none'
-                    label="Password" 
-                    value={password} 
-                    onChangeText={setPassword}
-                />
-               { state.errorMessage ? <Text style={styles.errorMsg}> {state.errorMessage} </Text> : null }
-                <Spacer>
-                    <Button onPress={() => signup({ email, password })} title="Sign Up"/>
-                </Spacer>
-                <TouchableOpacity onPress={() => navigation.navigate('Signin')}>
-                    <Text style={styles.link}>Already have an account? Sign in.</Text>
-                </TouchableOpacity>
+    return <SafeAreaView  style={styles.container} >
+            <AuthForm 
+                headerText="Sign up for Tracker"
+                errorMessage={state.errorMessage}
+                submitButtonText="Sign up"
+                onSubmit={signup}
+            />
+            <NavLink 
+                text="Already have an account? Sign in."
+                routeName="Signin"
+            />
+            
     </SafeAreaView>
 }
 
@@ -52,15 +33,6 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         marginBottom: 90
-    },
-    errorMsg: {
-        fontSize: 16,
-        color: 'red',
-        textAlign: 'center'
-    },
-    link: {
-        color: 'blue',
-        textAlign: 'center'
     }
 })
 
